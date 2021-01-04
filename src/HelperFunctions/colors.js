dfd286d63ec1fc1210808f32f462df16bf2975e5
dyson1602 <cclark000@citymail.cuny.edu>
2021-01-03 20:27:21 -0500

    navbar

--- src/HelperFunctions/colors.js
+++ src/HelperFunctions/colors.js
@@ -1,9 +1,9 @@
 export const SORTED_COLOR = "#545f66ff" //black-coral
 export const INITIAL_COLOR = "#829399ff" //cadet-grey
-export const PIVOT_COLOR = "#89bbfeff" //light-cyan "#d0f4eaff"
+export const PIVOT_COLOR = "pink" //light-cyan "#d0f4eaff"
 export const FADED_COLOR = "pink" //tea-green "#e8fcc2ff"
 export const COMPARE_COLOR = "#b1cc74ff" //middle-green-yellow
-export const NAVBAR_COLOR = "#89bbfeff" //french blue
+export const NAVBAR_COLOR = "#477890ff" //french blue
 export const MAIN_COLOR = "#f8ffe5ff"
 
 // french #89bbfeff
