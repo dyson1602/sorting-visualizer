6009cde43f0c9945a651ff21f207088c1e36a248
dyson1602 <cclark000@citymail.cuny.edu>
2021-01-07 17:06:39 -0500

    test index.html

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from "redux"
 import App from "./App"
 import "./index.css"
 import rootReducer from "./Redux/rootReducer"
-import reportWebVitals from "./reportWebVitals"
+
 
 const store = createStore(
   rootReducer,
@@ -23,8 +23,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
